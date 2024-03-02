0ce68046f923c43aea6db40a030d64f5c91428a2
bogda <bogdashka04@gmail.com>
2024-03-02 19:38:23 +0300

    commit5

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -1,5 +1,5 @@
 import flask
-from flask import jsonify
+from flask import jsonify, request
 
 from . import db_session
 from .jobs import Jobs
@@ -8,9 +8,18 @@ from .jobs import Jobs
 blueprint = flask.Blueprint('jobs_api', __name__, template_folder='templates', static_folder="static")
 
 
-@blueprint.route("/api/jobs")
+@blueprint.route("/api/jobs", method=["GET"])
 def get_jobs():
     db_sess = db_session.create_session()
     jobs = db_sess.query(Jobs).all()
     temp = [item.to_dict() for item in jobs]
     return jsonify({"jobs": temp})
+
+
+@blueprint.route("/api/jobs/<int:job_id>", method=["GET"])
+def get_job(job_id):
+    db_sess = db_session.create_session()
+    job = db_sess.query(Jobs).get(job_id)
+    if not job:
+        return jsonify({"error": "Bad request"})
+    return jsonify({"job": job.to_dict()})
