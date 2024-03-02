24b44195c4e0cf299731807bac67f7c565a8cfce
bogda <bogdashka04@gmail.com>
2024-03-02 19:46:18 +0300

    commit5

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -23,3 +23,25 @@ def get_job(job_id):
     if not job:
         return jsonify({"error": "Bad request"})
     return jsonify({"job": job.to_dict()})
+
+
+@blueprint.route("/api/jobs", method=["POST"])
+def create_job():
+    if not request.json:
+        return jsonify({"error": "No json file"})
+    if not all([key in request.json for key in ["team_leader", "job", "work_size", "collaborators"]]):
+        return jsonify({"error": "No keys"})
+    db_sess = db_session.create_session()
+    job = Jobs(
+        id=request.json["id"],
+        team_leader=request.json["team_leader "],
+        job=request.json["job"],
+        work_size=request.json["work_size"],
+        collaborators=request.json["collaborators"],
+        start_date=request.json["start_date"],
+        end_date=request.json["end_date"],
+        is_finished=request.json["is_finished"]
+    )
+    db_sess.add(job)
+    db_sess.commit()
+    return jsonify({"ok": "Everything is ok"})
